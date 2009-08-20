f27b0eba623706e3f57ac47bcea2cf1e1b7fdd45
sergeybe@gmail.com <sergeybe@gmail.com@95015238-8795-11de-a133-9d545b4d7674>
2009-08-20 17:27:17 +0000

    Added image for quit menu item. Clear up imports.

--- trunk/src/org/angdroid/angband2/AngbandActivity.java
+++ trunk/src/org/angdroid/angband2/AngbandActivity.java
@@ -27,7 +27,6 @@ import java.util.zip.ZipInputStream;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
