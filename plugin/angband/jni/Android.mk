dea3eb60e204f731d7f03af9e3c1cb840e38e75c
dtvernon <dtvernon@95015238-8795-11de-a133-9d545b4d7674>
2010-12-24 21:17:12 +0000

    update to angband 3.2

--- plugin/angband/jni/Android.mk
+++ plugin/angband/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := angband
-LOCAL_CFLAGS := -DUSE_AND -DANGDROID_ANGBAND_PLUGIN
+LOCAL_CFLAGS := -DUSE_AND -DANGDROID_ANGBAND_PLUGIN -std=c99
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../include \
 $(LOCAL_PATH)/../extsrc/src
@@ -13,6 +13,8 @@ LOCAL_LDLIBS := -llog
 LOCAL_SRC_FILES := \
 ../../common/angdroid.c \
 ../extsrc/src/player/calcs.c \
+../extsrc/src/player/player.c \
+../extsrc/src/player/spell.c \
 ../extsrc/src/player/timed.c \
 ../extsrc/src/player/util.c \
 ../extsrc/src/attack.c \
@@ -24,8 +26,7 @@ LOCAL_SRC_FILES := \
 ../extsrc/src/cmd2.c \
 ../extsrc/src/cmd3.c \
 ../extsrc/src/cmd4.c \
-../extsrc/src/cmd5.c \
-../extsrc/src/cmd-know.c \
+../extsrc/src/cmd-misc.c \
 ../extsrc/src/cmd-obj.c \
 ../extsrc/src/death.c \
 ../extsrc/src/debug.c \
@@ -36,11 +37,12 @@ LOCAL_SRC_FILES := \
 ../extsrc/src/game-event.c \
 ../extsrc/src/generate.c \
 ../extsrc/src/history.c \
-../extsrc/src/init1.c \
 ../extsrc/src/init2.c \
 ../extsrc/src/load.c \
 ../extsrc/src/load-old.c \
+../extsrc/src/macro.c \
 ../extsrc/src/option.c \
+../extsrc/src/parser.c \
 ../extsrc/src/pathfind.c \
 ../extsrc/src/prefs.c \
 ../extsrc/src/randname.c \
@@ -56,24 +58,29 @@ LOCAL_SRC_FILES := \
 ../extsrc/src/tables.c \
 ../extsrc/src/target.c \
 ../extsrc/src/trap.c \
-../extsrc/src/ui-birth.c \
 ../extsrc/src/ui.c \
-../extsrc/src/ui-event.c \
+../extsrc/src/ui-birth.c \
+../extsrc/src/ui-knowledge.c \
 ../extsrc/src/ui-menu.c \
+../extsrc/src/ui-options.c \
+../extsrc/src/ui-spell.c \
 ../extsrc/src/util.c \
 ../extsrc/src/variable.c \
 ../extsrc/src/wizard.c \
 ../extsrc/src/wiz-spoil.c \
 ../extsrc/src/wiz-stats.c \
+../extsrc/src/x-char.c \
 ../extsrc/src/x-spell.c \
 ../extsrc/src/xtra2.c \
 ../extsrc/src/xtra3.c \
+../extsrc/src/z-bitflag.c \
 ../extsrc/src/z-file.c \
 ../extsrc/src/z-form.c \
 ../extsrc/src/z-msg.c \
 ../extsrc/src/z-quark.c \
 ../extsrc/src/z-rand.c \
 ../extsrc/src/z-term.c \
+../extsrc/src/z-textblock.c \
 ../extsrc/src/z-type.c \
 ../extsrc/src/z-util.c \
 ../extsrc/src/z-virt.c \
@@ -89,8 +96,6 @@ LOCAL_SRC_FILES := \
 ../extsrc/src/object/obj-ui.c \
 ../extsrc/src/object/obj-util.c \
 ../extsrc/src/object/randart.c \
-#../extsrc/src/z-bitflag.c # exists in svn head, but not in angband-3.1.2v2
-# see http://trac.rephial.org/browser/tags/angband-3.1.2v2/src
 
 #include $(BUILD_STATIC_LIBRARY)
 include $(BUILD_SHARED_LIBRARY)
