e8b327cdfa10bd9760c680ed8d03a1314e102b07
dtvernon <dtvernon@95015238-8795-11de-a133-9d545b4d7674>
2011-02-15 04:12:20 +0000

    3.2 borg repair

--- trunk/plugin/angband/patch/copy/src/borg1.h
+++ trunk/plugin/angband/patch/copy/src/borg1.h
@@ -2032,15 +2032,6 @@ extern glyph translate_visuals[255][255];
 
 #endif /* ALLOW_BORG_GRAPHICS */
 
-// some missing macros... 
-// how does everyone else compile the borg without these?
-// -----------------------------------------------------------
-/* Allocate a wiped array of type T[N], assign to pointer P */
-#define C_MAKE(P, N, T) \
-	((P) = C_ZNEW(N, T))
-/* Allocate a wiped thing of type T, assign to pointer P */
-#define MAKE(P, T) \
-	((P) = ZNEW(T))
 
 #endif
 
