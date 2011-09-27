da97227bc6d896a5c8997762a2d864a169e17d64
david20708@gmail.com <david20708@gmail.com@95015238-8795-11de-a133-9d545b4d7674>
2011-09-27 01:48:15 +0000

    Changes for upstream 3.3.1 release.
    I had to remove borg from the angband app because it wasn't compatible
    with 3.3.

--- find-src.sh
+++ find-src.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dir=plugin/nightly
+dir=plugin/$1
 echo 'LOCAL_SRC_FILES += \' > $dir/jni/Makefile.src
 for f in $(perl -pe 's/\\\n//g' $dir/extsrc/src/Makefile.src | perl -ne 'if(/^ANGFILES =|^ZFILES =/) { s/^.*=//; s/\.o/.c/g; print }'); do echo "../extsrc/src/$f \\"; done >> $dir/jni/Makefile.src
 echo '' >> $dir/jni/Makefile.src
