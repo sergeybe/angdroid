359421e8804957d82d51ce2d396f2bbb03b30a4d
david20708@gmail.com <david20708@gmail.com@95015238-8795-11de-a133-9d545b4d7674>
2011-11-25 07:43:39 +0000

    modified not to patch backup files (and backups of backup files)

--- fix-angband.sh
+++ fix-angband.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . ~/code/secret-keys.sh
-grep -rl '%SL_SECRET%' src/org/angdroid | xargs perl -pi~ -e "s/%SL_SECRET%/$SL_SECRET_ANGBAND/"
+grep -rl '%SL_SECRET%' src/org/angdroid | grep -v '~$' | xargs perl -pi~ -e "s/%SL_SECRET%/$SL_SECRET_ANGBAND/"
