99c4fbf431d18d09a60f5d21173baf077ef16949
david20708@gmail.com <david20708@gmail.com@95015238-8795-11de-a133-9d545b4d7674>
2011-11-28 06:52:00 +0000

    adding scoreloop support to nightly

--- trunk/fix-nightly.sh
+++ trunk/fix-nightly.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-grep -rl org\.angdroid\.angband . | xargs perl -pi~ -e "s/org\.angdroid\.angband/org\.angdroid\.nightly/"
+. ~/code/secret-keys.sh
+egrep -rl 'org\.angdroid\.angband|%SL_SECRET%' . | grep -v '~$' | xargs perl -pi~ -e "s/org\.angdroid\.angband/org\.angdroid\.nightly/; s/%SL_SECRET%/$SL_SECRET_ANGBAND/"
 rm -rf src/org/angdroid/nightly
 mv src/org/angdroid/angband src/org/angdroid/nightly
