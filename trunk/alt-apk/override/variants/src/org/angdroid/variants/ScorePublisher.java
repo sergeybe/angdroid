94f3abf9173abc9e3f375c1a2333b2f41528e951
dtvernon <dtvernon@95015238-8795-11de-a133-9d545b4d7674>
2012-11-05 00:22:34 +0000

    temporarily disable scoreloop in variants & classics -- builds work now.

---
+++ trunk/alt-apk/override/variants/src/org/angdroid/variants/ScorePublisher.java
@@ -0,0 +1,30 @@
+package org.angdroid.variants;
+
+import android.app.Activity;
+
+public class ScorePublisher {
+
+	public ScorePublisher(Activity a)
+	{
+	}
+
+	public static void Init(android.app.Application app)
+	{
+	}
+
+	public static void Destroy()
+	{
+	}
+
+	public void ShowLeaderboards()
+	{
+	}
+
+	public void ShowEntry()
+	{
+	}
+
+	public void Publish(ScoreContainer curScore)
+	{
+	}
+}
